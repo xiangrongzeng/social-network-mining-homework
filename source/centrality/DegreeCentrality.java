742ae76d1e8772f9b2ba3e7ae538da0d5bcf0c3b
sunder <rxiangz@gmail.com>
2015-04-08 17:28:44 +0800

    change the function name to "get"

--- source/centrality/DegreeCentrality.java
+++ source/centrality/DegreeCentrality.java
@@ -4,14 +4,11 @@ import centrality.AdjacentMatrix;
 
 class DegreeCentrality {
 
-    static int[] calculate(){
-        AdjacentMatrix adjm = new AdjacentMatrix();
-        int[] centralities = {0,0,0,0,0,0,0,0,0,0};
-        for (int node=0; node<adjm.getLength(); node++){
-            for (int j=0; j<adjm.getWidth(); j++) {
-                centralities[node] += adjm.get(node, j);
-            }
+    static int get(int node){
+        int centralitie = 0;
+        for (int j=0; j<AdjacentMatrix.getLength(); j++) {
+            centralitie += AdjacentMatrix.get(node, j);
         }
-        return centralities;
+        return centralitie;
     }
 }

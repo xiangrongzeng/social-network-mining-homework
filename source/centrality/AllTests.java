94a68cf950cb8ea62e4a41271d9520470640be35
sunder <rxiangz@gmail.com>
2015-04-10 13:20:06 +0800

    add the test of betweennesscentrality finished all tests

--- source/centrality/AllTests.java
+++ source/centrality/AllTests.java
@@ -6,12 +6,13 @@ class AllTests {
     static final String NEWLINE = "\r\n";
     public static void main(String[] args) {
 //        testMatrix();
-//        testDegreeCentrality();
-//        testEigenvectorCentrality();
-//        testKatzCentrality();
-//        testPagerankCentrality();
-        testGraph();
+        testDegreeCentrality();
+        testEigenvectorCentrality();
+        testKatzCentrality();
+        testPagerankCentrality();
+//        testGraph();
         testClosenessCentrality();
+        testBetweennessCentrality();
     }
 
     static void testMatrix(){
@@ -26,25 +27,32 @@ class AllTests {
     }
 
     static void testDegreeCentrality(){
+        System.out.println("-------------------------------------");
+        System.out.println("DegreeCentrality");
         for (int node = 0; node <AdjacentMatrix.getLength(); node++){
             System.out.println(DegreeCentrality.get(node));
         }
     }
 
     static void testEigenvectorCentrality(){
-        System.out.println(EigenvectorCentrality.getLargestEigenvalueNumber());
+        System.out.println("-------------------------------------");
+        System.out.println("EigenvectorCentrality");
         for (int node = 0; node <AdjacentMatrix.getLength(); node++){
             System.out.println(EigenvectorCentrality.get(node));
         }
     }
 
     static void testKatzCentrality(){
+        System.out.println("-------------------------------------");
+        System.out.println("KatzCentrality");
         for (int node = 0; node <AdjacentMatrix.getLength(); node++){
             System.out.println(KatzCentrality.get(node));
         }
     }
 
     static void testPagerankCentrality(){
+        System.out.println("-------------------------------------");
+        System.out.println("PagerankCentrality");
         for(int node = 0; node < AdjacentMatrix.getLength(); node++){
             System.out.println(PagerankCentrality.get(node));
         }
@@ -56,8 +64,18 @@ class AllTests {
     }
 
     static void testClosenessCentrality(){
+        System.out.println("-------------------------------------");
+        System.out.println("ClosenessCentrality");
         for (int node = 0; node < Graph.getNumberOfVertexes(); node++){
             System.out.println(ClosenessCentrality.get(node));
+        }
+    }
+
+    static void testBetweennessCentrality(){
+        System.out.println("-------------------------------------");
+        System.out.println("BetweennessCentrality");
+        for(int node = 0; node < AdjacentMatrix.getLength(); node++){
+            System.out.println(BetweennessCentrality.get(node));
         }
     }
 }

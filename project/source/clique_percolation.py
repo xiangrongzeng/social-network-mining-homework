fb428c9e2df6b8f6941b4d04f19dcab5840f2dfc
sunder <rxiangz@gmail.com>
2015-04-29 22:35:51 +0800

    改善性能：性能瓶颈在merge_clique函数。在k为3时，clique的个数是1612010，而该函数的需循环的次数大约是1612010^2次,复杂度为O(n^2)。重写了这个函数，使得复杂度为O(n)。但是不确定是否存在bug。小规模验证通过，大规模验证存疑

--- project/source/clique_percolation.py
+++ project/source/clique_percolation.py
@@ -3,57 +3,73 @@ import itertools
 import re
 import networkx as nx
 import matplotlib.pyplot as plt
+from operator import itemgetter
 
 def clique_percolation(k, filepath):
     print 'create graph'
     my_graph = get_graph(filepath)
+    sorted(my_graph.iteritems(), key=itemgetter(0), reverse=False)
     cliques = []
     print 'find cliques'
     find_k_clique(k, my_graph, cliques)
+#    print cliques
+    print 'merge cliques',len(cliques)
     clusters = merge_clique(cliques)
 #    print clusters
     print 'draw graph'
     draw_cluster_network(my_graph,clusters)
 
+    write_clusters(clusters)
+
+def write_clusters(clusters):
+    f = open('clusters.txt','a')
+    for cluster in clusters:
+        line = ''
+        for node in cluster:
+            line += node + ','
+        line = re.sub(',$','\n',line)
+        f.write(line)
+    f.close()
+
 def merge_clique(cliques):
-    cluster_label = []
-    for i in range(0, len(cliques)):
-        cluster_label.append(i)
-
-    clusters = [[]]*len(cliques)
-    for i in range(0, len(cliques)):
-        for j in range( i+1, len(cliques)):
-#            print i,cliques[i],j,cliques[j]
-            if is_nearby(cliques[i], cliques[j]) == True:
-                cluster_label[j] = cluster_label[i]
-                # 去重
-                clique =clusters[cluster_label[i]] + cliques[i] + cliques[j]
-                clusters[cluster_label[i]] =  list(set(clique))
-#                print cluster_label[i],clusters
-
-    while [] in clusters:
-        clusters.remove([])
-
+    clusters = [cliques[0]]
+    for i in range(1, len(cliques)):
+        if i%20000 == 0:
+            print i,1.0*i/len(cliques)
+        clique = cliques[i]
+        for j in range(0,len(clusters)):
+            cluster = clusters[j]
+            if is_nearby(clique, cluster) == True:
+                cluster += clique
+                clusters[j] = list(set(cluster))
+                break
+            if j == len(clusters)-1:
+                clusters.append(clique)
     return clusters
 
 def is_nearby(clique1, clique2):
     clique = clique1 + clique2
     clique = list(set(clique))
-    if len(clique) == len(clique1) + 1:
+    if len(clique) <= len(clique2) + 1:
         return True
     else:
         return False
 
 def find_k_clique(k, graph, cliques):
+    count = 0
     for central_node,adjacent_nodes in graph.items():
         if len(adjacent_nodes) >= k-1:
             for nodes in (itertools.combinations(adjacent_nodes,k-1)):
+##                print central_node,nodes
+                count += 1
                 if is_complete(nodes,graph) == True:
+                    count
                     new = [central_node]
                     for node in nodes:
                         new.append(node)
-#                    print new
+##                    print new
                     cliques.append(new)
+    print count
 
 
 def is_complete(nodes, graph):
@@ -79,7 +95,6 @@ def get_graph(filepath):
     return my_graph
 
 def draw_cluster_network(graph,clusters):
-    colors = ['y','b']
     original_graph = nx.Graph()
     cluster_graph = nx.Graph()
     for node,nodes in graph.items():
@@ -87,9 +102,10 @@ def draw_cluster_network(graph,clusters):
             cluster_graph.add_edge(node, v)
             original_graph.add_edge(node, v)
     pos = nx.spring_layout(cluster_graph)
+    colors = ['red','blue','black','white','yellow','green','gray','cyan','magenta']
     for i in range(0, len(clusters)):
-        col = colors[i]
-        nx.draw_networkx_nodes(cluster_graph,pos,clusters[i],node_color=col)
+        nx.draw_networkx_nodes(cluster_graph,pos,clusters[i],node_color=colors[i])
+#        nx.draw_networkx_nodes(cluster_graph,pos,clusters[i],node_color=(1.0*i/len(clusters),1.0*i/len(clusters),1.0*i/len(clusters)))
 
 
     nx.draw_networkx_edges(cluster_graph,pos, with_labels=True,alpha=0.5)

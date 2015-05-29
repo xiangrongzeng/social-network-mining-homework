c98c99e5bbce8edea9454ba5901d4b91c004ee7c
sunder <rxiangz@gmail.com>
2015-05-29 10:17:40 +0800

    添加了节点标签，并将节点透明度设置为0.5

--- project/source/draw_communities.py
+++ project/source/draw_communities.py
@@ -26,31 +26,37 @@ def get_clusters(communities_data_path):
         clusters.append(cluster)
     return clusters
 
-
 def draw_cluster_network(graph_data_path, communities_data_path):
     graph = get_graph(graph_data_path)
     clusters = get_clusters(communities_data_path)
     original_graph = nx.Graph()
     cluster_graph = nx.Graph()
+    labels = {}
     for node,nodes in graph.items():
         for v in nodes:
             cluster_graph.add_edge(node, v)
             original_graph.add_edge(node, v)
-    pos = nx.spring_layout(cluster_graph)
+            labels[node] = r'$'+node+'$'
+            labels[v] = r'$'+v+'$'
     pos = nx.shell_layout(cluster_graph)
+    pos = nx.spectral_layout(cluster_graph)
+    pos = nx.spring_layout(cluster_graph)
     for i in range(0, len(clusters)):
         nx.draw_networkx_nodes(cluster_graph,
                                pos,
-                               node_size = 50,
+                               node_size = 500,
                                nodelist = clusters[i],
-                               node_color=(random.random(),random.random(),random.random()))
-
-
+                               node_color=(random.random(),random.random(),random.random()),
+                               alpha = 0.5)
     nx.draw_networkx_edges(cluster_graph,
                            pos,
                            with_labels=True,
                            alpha=0.5,
                            width = 0.5)
+    nx.draw_networkx_labels(cluster_graph,
+                            pos,
+                            labels,
+                            fontsize=16)
 #    nx.draw(original_graph)
     plt.show()
     plt.savefig('2.png')
